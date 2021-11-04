6d190111346a76cba9ea6ae798474d3c5ce566ca
Franky Nicoletta <fnicoletta97@gmail.com>
2021-11-04 16:04:53 -0700

    finished basic styling of mobile navigation

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Nav.module.scss";
 
 const navItems = ["Home", "About", "Products", "Contact"];
 
 const Nav: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleOpenNav = () => setIsMenuOpen((prev) => !prev);
+
   return (
     <nav className={styles.container}>
       <div className={styles.imgContainer}>
@@ -15,11 +19,19 @@ const Nav: React.FC = () => {
           height="150"
         />
       </div>
-      <ul className={styles.navList}>
-        {navItems.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
+      <ul className={styles.hamburger} onClick={handleOpenNav}>
+        <li className={styles.line}></li>
+        <li className={styles.line}></li>
+        <li className={styles.line}></li>
       </ul>
+
+      <div className={isMenuOpen ? styles.openMenu : styles.closeMenu}>
+        <ul className={styles.navList}>
+          {navItems.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
